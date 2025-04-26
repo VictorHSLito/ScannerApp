78f4e62a9baa5761637405053cd54986e1e25f4a
Victor Hugo <victorferrarihugo@hotmail.com>
2025-04-26 14:47:20 -0300

    feat: Create scaffold of ListItemsUsecase

--- src/app/domain/interfaces/IItemRepository.ts
+++ src/app/domain/interfaces/IItemRepository.ts
@@ -2,7 +2,7 @@ import { Item } from "../entities/Item";
 
 export interface IItemRepository {
     saveItem(item: Item): Promise<Item | null>;
-    listItems(): Item[];
+    listItems(): Promise<Item[] | null>;
     removeItem(): void;
     cleanItems(): void;
 }
