d25ef4e1413993a09d4e4ecf412ef27c8455c66d
Victor Hugo <victorferrarihugo@hotmail.com>
2025-05-08 21:21:36 -0300

    feat: Update and Create remaining use cases

--- src/app/domain/usecases/RemoveItemUseCase.ts
+++ src/app/domain/usecases/RemoveItemUseCase.ts
@@ -1,3 +1,21 @@
+import { IItemRepository } from "../interfaces/IItemRepository";
+
 export class RemoveItemUseCase {
-    
+    private _repo: IItemRepository;
+
+    constructor (repo: IItemRepository) {
+        this._repo = repo;
+    }
+
+    async execute(id: string): Promise<void> {
+        try {
+            if (!id) {
+                throw new Error("ID wasn't provided or is incorrect");
+            }
+            await this._repo.removeItem(id);
+        } catch (error) {
+            console.error("Failed to remove item:", error);
+            throw new Error("An error occurred while removing the item. Please try again.");
+        }
+    }
 }
