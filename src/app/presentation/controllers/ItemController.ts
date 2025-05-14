03c8b2972797506a40ebf6c95ce5e3f4344c29f9
Victor Hugo <victorferrarihugo@hotmail.com>
2025-05-13 21:02:18 -0300

    fix: Fix and adjusts some classes

--- src/app/presentation/controllers/ItemController.ts
+++ src/app/presentation/controllers/ItemController.ts
@@ -1,18 +1,33 @@
-import { ItemLocalDataSource } from "../../data/datasources/local/ItemLocalDataSource";
+import { ItemMapper } from "../../data/mappers/ItemMapper";
 import { ItemModel } from "../../data/model/ItemModel";
+import { SQLiteItemRepository } from "../../data/repositories/SQLiteItemRepository";
+import { Item } from "../../domain/entities/Item";
+import { AddItem } from "../../domain/usecases/item/AddItemUseCase";
+import { ClearListItems } from "../../domain/usecases/item/ClearListItemsUseCase";
+import { ListItems } from "../../domain/usecases/item/ListItemsUseCase";
+import { RemoveItem } from "../../domain/usecases/item/RemoveItemUseCase";
 
 export class ItemController {
-    private dataSource = new ItemLocalDataSource;
+    private sql = new SQLiteItemRepository;
+    private addUseCase = new AddItem(this.sql);
+    private listUseCase = new ListItems(this.sql);
+    private deleteUseCase = new RemoveItem(this.sql);
+    private clearListUseCase = new ClearListItems(this.sql);
 
-    async getAllItems(): Promise<ItemModel[]> {
-        return await this.dataSource.listItems();
+    async getAllItems(): Promise<Item[] | null> {
+        return await this.listUseCase.execute();
     }
 
-    async saveItem(item: ItemModel): Promise<void> {
-        return await this.dataSource.saveItem(item);
+    async saveItem(item: ItemModel) {
+        const itemModel = ItemMapper.toEntity(item);
+        return await this.addUseCase.execute(itemModel);
     }
 
     async deleteItem(id: string): Promise<void> {
-        return await this.dataSource.removeItem(id);
+        return await this.deleteUseCase.execute(id);
+    }
+
+    async clearList(): Promise<void> {
+        return await this.clearListUseCase.execute();
     }
 }
